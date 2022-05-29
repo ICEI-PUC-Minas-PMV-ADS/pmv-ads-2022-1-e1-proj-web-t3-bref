8f4add704bae25fa2906ba2221811be0665b587b
LucasCNS <celionuvem@gmail.com>
2022-05-28 22:54:23 -0300

    DEV - Organizando variáveis e criando funções pais e filhas

--- src/code/scripts/entrar.js
+++ src/code/scripts/entrar.js
@@ -1,25 +1,38 @@
-const $ = document.querySelector.bind(document)
-
-function NavegacaoAbas() {
+function Entrar() {
+	/* Ver senhas */
+	const btn = document.querySelector(".fa-eye");
+	const btnSenhaCadastro = document.querySelector("#verSenha")
+	const btnConfirm = document.querySelector("#verConfirmarSenha")
+	/* Inputs */
+	const inputSenha = document.querySelector("#senha")
+	const inputSenhaCadastro = document.querySelector("#senhaCadastro")
+	const inputConfirmarSenha = document.querySelector("#confirmar-senha")
+	/* Comparação das Senhas */
+	const confirmarSenha = document.querySelector("#confirmar-senha")
+	
+	function NavegacaoAbas() {
     
     const html = {
-        links: [...$('.links-abas').children],
-        contents: [...$('.conteudo-abas').children],
-        openTab: $('[data-open]')
+			links: [...document.querySelector('.links-abas').children],
+			contents: [...document.querySelector('.conteudo-abas').children],
+			openTab: document.querySelector('[data-open]')
     }
 
     function esconderTodoConteudoAbas (){
-        html.contents.forEach(section => {
-            section.style.display = "none"
-        })
+			html.contents.forEach(section => {
+					section.style.display = "none"
+			})
     }
 
     function mostrarAbaAtual(id){
-        const tabcontent = $('#' + id)
-        tabcontent.style.display = "block"
+
+			const tabcontent = document.querySelector('#' + id);
+			
+			tabcontent.style.display = "block"
     }
 
     function selecionarAba(event) {
+
         esconderTodoConteudoAbas()
 
         const target = event.currentTarget
@@ -42,57 +55,91 @@ function NavegacaoAbas() {
     return {
         iniciar
     }
-}
+	}
 
-function MostrarSenhaEntrar() {
-	  
-	let btn = document.querySelector(".fa-eye")
-	
-		btn.addEventListener("click", ()=>{
-			let inputSenha = document.querySelector("#senha")
+	function MostrarSenhas() {
 
-			if(inputSenha.getAttribute("type") == "password"){
-					inputSenha.setAttribute("type" , "type")
+		function mostrarSenhaEntrar() {
+			
+				btn.addEventListener("click", ()=>{
+					
+					if(inputSenha.getAttribute("type") == "password"){
+							inputSenha.setAttribute("type" , "type")
+						} else {
+							inputSenha.setAttribute("type", "password")
+						}
+				}) 
+		}
+
+		function mostrarSenhaCadastro() {
+			
+			btnSenhaCadastro.addEventListener("click", ()=>{
+
+				if(inputSenhaCadastro.getAttribute("type") == "password"){
+						inputSenhaCadastro.setAttribute("type" , "type")
 				} else {
-					inputSenha.setAttribute("type", "password")
+						inputSenhaCadastro.setAttribute("type", "password")
 				}
-		}) 
-}
+			})
+		}
 
-function MostrarSenhaCadastro() {
+		function mostrarConfirmarSenhaCadastro() {
 
-	let btnConfirm = document.querySelector("#verConfirmarSenha")
+				btnConfirm.addEventListener("click", ()=>{
 
-		btnConfirm.addEventListener("click", ()=>{
-			let inputConfirmarSenha = document.querySelector("#confirmar-senha")
+					if(inputConfirmarSenha.getAttribute("type") == "password"){
+							inputConfirmarSenha.setAttribute("type" , "type")
+					} else {
+							inputConfirmarSenha.setAttribute("type", "password")
+					}
+				})
+		}
 
-			if(inputConfirmarSenha.getAttribute("type") == "password"){
-					inputConfirmarSenha.setAttribute("type" , "type")
+		function iniciar(){
+			mostrarSenhaEntrar()
+			mostrarSenhaCadastro()
+			mostrarConfirmarSenhaCadastro()
+
+		}
+
+		return {
+				iniciar
+		}
+	}
+
+	function compararSenhas() {
+
+		confirmarSenha.addEventListener("keyup", () => {
+			if(inputSenhaCadastro.value != confirmarSenha.value) {
+				confirmarSenha.setAttribute("style", "border-color: red")
 			} else {
-					inputConfirmarSenha.setAttribute("type", "password")
+				confirmarSenha.setAttribute("style", "border-color: green")
 			}
 		})
-}
+	}
 
-function ValidarSenha() {
-	let senha = $("#senhaCadastro")
-	let confirmarSenha = $("#confirmar-senha")
+	function cadastrar() {
+	
+	}
 
-	confirmarSenha.addEventListener("keyup", () => {
-		if(senha.value != confirmarSenha.value) {
-			confirmarSenha.setAttribute("style", "border-color: red")
-		} else {
-			confirmarSenha.setAttribute("style", "border-color: green")
-		}
-	})
-}
+	function iniciar() {
+
+		const navegacaoAbas = NavegacaoAbas()
+		navegacaoAbas.iniciar();
+
+		const mostrarSenhas = MostrarSenhas()
+		mostrarSenhas.iniciar();
+		
+		compararSenhas();
+		cadastrar();
+	}
+
+	return {
+		iniciar
+	}
+}	
 
 window.addEventListener('load', () => {
-	const navegacaoAbas = NavegacaoAbas()
-	navegacaoAbas.iniciar()
-	MostrarSenhaEntrar();
-	MostrarSenhaCadastro();
-	ValidarSenha();
-
+	const entrar = Entrar()
+	entrar.iniciar()
 })
-
