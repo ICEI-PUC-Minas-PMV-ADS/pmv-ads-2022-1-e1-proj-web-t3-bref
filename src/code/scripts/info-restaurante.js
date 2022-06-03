392d8c6056861af1b50630381f0787c53202522c
Bruno Ruas <bruno.ruas2@gmail.com>
2022-06-03 10:16:28 -0400

    I DID IT! full solution to read an url param, read the .json storage and create a restaurant object

--- src/code/scripts/info-restaurante.js
+++ src/code/scripts/info-restaurante.js
@@ -1,7 +1,30 @@
-// gtting the id from URL
+// Sequestrando a funcao console.log pra poder capurar os prints em um array
+// https://stackoverflow.com/questions/52347756/read-console-log-output-form-javascript
+var consoleStorage = [];
+
+console.log = function(msg){
+   consoleStorage.push(msg);
+   console.warn(msg); // if you need to print the output
+}
+
+// getting the id from URL
 var id = parseInt(document.location.toString().replace("http://127.0.0.1:5501/src/code/html/info-restaurante.html?id=",''))
 
-// read .json file as a objetct
-const response = await fetch("../database/restaurants.json");
-const restaurantsDatabaseJson = await response.json();
-let rest = restaurantsDatabaseJson.restaurants[id-1];
+// https://dev.to/ramonak/javascript-how-to-access-the-return-value-of-a-promise-object-1bck
+const readDB = fetch('../database/restaurants.json')
+    .then((response) => response.json())
+    .then((database => {
+        return database.restaurants[id];
+}));
+
+const printRest = () => {
+    readDB.then((a) => {
+        console.log(a);
+    })
+};
+
+// start sequence
+// the restaurant information is acessable to consoleStorage[0]
+window.onload = function() {
+    printRest();
+}
