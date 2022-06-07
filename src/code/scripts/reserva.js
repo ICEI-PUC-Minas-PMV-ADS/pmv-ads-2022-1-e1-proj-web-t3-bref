43b65b70577d725b48dd6320744bd769fa481a3b
LucasCNS <celionuvem@gmail.com>
2022-06-07 20:25:38 -0300

    DEV - Padronizando layout da pg Reserva.

--- src/code/scripts/reserva.js
+++ src/code/scripts/reserva.js
@@ -34,3 +34,69 @@ function toggleButtonsDisable(){
 function validateEmail(email){
     return /\S+@\S+\.\S+/.test(email);
 }
+
+
+/* Aumentar e diminuir fonte */
+
+var maxClicksAddMoreSize = 6;
+var maxClicksSubtractMoreSize = -2;
+
+var countClicksToHiddenItems = 3;
+var defaultSizeWindow = 0;
+
+var countClicksChangeSizeItems = 0;
+var namesItemToHidden = ['null'];
+
+function startWindow() {
+    let currentDefaultWindowSize = getCurrentWindowSizeValue();
+    if (defaultSizeWindow == 0)
+        defaultSizeWindow = currentDefaultWindowSize;
+}
+
+function getCurrentWindowSizeValue() {
+    let myWindowSize = window.getComputedStyle(document.body).fontSize;
+    let currentDefaultWindowSize = parseInt(myWindowSize.replace('px', ''));
+
+    return currentDefaultWindowSize;
+}
+
+function changeSizeAllItems(isAddMoreSize) {
+    let currentWindowSize = getCurrentWindowSizeValue();
+    if (isAddMoreSize && isValidAddMoreSize()) {
+        countClicksChangeSizeItems++;
+        document.body.style.fontSize = (currentWindowSize + 3) + 'px';
+    }
+    if (!isAddMoreSize && isValidSubtractMoreSize()) {
+        countClicksChangeSizeItems--;
+        document.body.style.fontSize = (currentWindowSize - 3) + 'px';
+    }
+    actionAboutShowableItems(namesItemToHidden, countClicksChangeSizeItems, countClicksToHiddenItems);
+}
+
+function isValidAddMoreSize() {
+    return countClicksChangeSizeItems < maxClicksAddMoreSize;
+}
+
+function isValidSubtractMoreSize() {
+    return countClicksChangeSizeItems > maxClicksSubtractMoreSize;
+}
+
+function resetDocumentSizes() {
+    document.body.style.fontSize = defaultSizeWindow + 'px';
+    countClicksChangeSizeItems = 0;
+    showHiddenItems(namesItemToHidden, 'visible');
+}
+
+function actionAboutShowableItems(namesItemToHidden, currentCount, maxClicksToHiddenItems) {
+    let show = 'visible';
+    let notShow = 'hidden';
+    let action = currentCount >= maxClicksToHiddenItems ? notShow : show;
+    showHiddenItems(namesItemToHidden, action);
+}
+
+function showHiddenItems(namesItemToHidden, action) {
+    for (x = 0; namesItemToHidden.length > x; x++) {
+        let myBigBreff = document.getElementById(namesItemToHidden[x]);
+        myBigBreff.style.visibility = action;
+    }
+}
